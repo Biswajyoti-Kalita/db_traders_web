52d0c771e136378296cd4f3e447472f403836ed6
Biswajyoti-Kalita <biswajyotikalita142@gmail.com>
2022-03-23 23:19:18 +0530

    seed fixed

--- server/seed.js
+++ server/seed.js
@@ -1,8 +1,30 @@
-
-	const app = require('./app');
-	const db = require('./models');
-    const PasswordService = require('./services/passwordService');
-	async function createSeeds() {
-await db.user.create( {"id":1,"username":"admin","name":"admin","email":"admin@admin.in","password":"a12345","role_id":0} );await db.user.create( {"id":2,"username":"client","name":"client","email":"client@admin.in","password":"a12345","role_id":1} );await db.user.create( {"id":3,"username":"others","name":"others","email":"others@admin.in","password":"a12345","role_id":2} );
+const app = require("./app");
+const db = require("./models");
+const PasswordService = require("./services/passwordService");
+async function createSeeds() {
+  await db.user.create({
+    id: 1,
+    username: "admin",
+    name: "admin",
+    email: "admin@admin.in",
+    password: await PasswordService.hashPassword("a12345"),
+    role_id: 0,
+  });
+  await db.user.create({
+    id: 2,
+    username: "client",
+    name: "client",
+    email: "client@admin.in",
+    password: await PasswordService.hashPassword("a12345"),
+    role_id: 1,
+  });
+  await db.user.create({
+    id: 3,
+    username: "others",
+    name: "others",
+    email: "others@admin.in",
+    password: await PasswordService.hashPassword("a12345"),
+    role_id: 2,
+  });
 }
 createSeeds();
